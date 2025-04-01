f7a5728f1e2be00e5af858e136dc0b36a2a2e498
shoyo <shoyo2209@gmail.com>
2025-04-02 04:15:04 +0500

    chore: implement cors and rate-limit

--- dist/index.js
+++ dist/index.js
@@ -9,7 +9,30 @@ const auth_routes_1 = require("./presentation/routes/auth.routes");
 const course_routes_1 = require("./presentation/routes/course.routes");
 const user_routes_1 = require("./presentation/routes/user.routes");
 const error_middleware_1 = require("./presentation/middlewares/error.middleware");
+const cors_1 = __importDefault(require("cors"));
+const express_rate_limit_1 = __importDefault(require("express-rate-limit"));
 const app = (0, express_1.default)();
+const allowedOrigins = [
+    "http://localhost:5173/",
+    "https://q-univer-test-front.vercel.app/",
+];
+const limiter = (0, express_rate_limit_1.default)({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: "Too many requests from this IP, please try again later.",
+});
+app.use((0, cors_1.default)({
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        }
+        else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true,
+}));
+app.use(limiter);
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
 app.use("/uploads", express_1.default.static("public/uploads"));
--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,35 @@ import { authRoutes } from "./presentation/routes/auth.routes";
 import { courseRoutes } from "./presentation/routes/course.routes";
 import { userRoutes } from "./presentation/routes/user.routes";
 import { errorMiddleware } from "./presentation/middlewares/error.middleware";
+import cors from "cors";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173/",
+  "https://q-univer-test-front.vercel.app/",
+];
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: "Too many requests from this IP, please try again later.",
+});
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+app.use(limiter);
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static("public/uploads"));
